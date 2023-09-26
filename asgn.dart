b0c02c6fc340757fb0b227ab079a8262204385b3
john ormartii <johnormartii@gmail.com>
2023-09-26 11:56:04 +0300

    this is my assignment sir

---
+++ asgn.dart
@@ -0,0 +1,23 @@
+//Do not forget to solve the question that we discussed in the DART CLASS Write a dart if else if statement showing your Maths marks in class 7. Above 80,print mwalimu wa math hapa ni wapi,70 and above print good job,60 and above print good trial,below 50 print work extra hard. Discuss in your peer groups.
+void main() {
+  var math1 = 86;
+  var math2 = 70;
+  var math3 = 62;
+  var math4 = 55;
+  if (math1 > 80) ;
+  print("mwalimu wa math hapa n wapi");
+  if (math2 > 70) ;
+  print("good job");
+  if (math3 > 60) ;
+  print("good trial");
+  if (math4 > 50) ;
+  print("work extra hard");
+  elseif(
+    math1,
+    math2,
+    math3,
+    math,
+  ) {
+    print("did not do the exam");
+  }
+}
